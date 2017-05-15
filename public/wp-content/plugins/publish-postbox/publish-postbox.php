62ae2642f22e387cfd536ec72fcb674c0012fa7b
Errol Sidelsky <errol@squie.com>
2017-05-15 12:15:05 +0100

    ACF removed

--- public/wp-content/plugins/publish-postbox/publish-postbox.php
+++ public/wp-content/plugins/publish-postbox/publish-postbox.php
@@ -41,4 +41,41 @@
     add_action( 'admin_enqueue_scripts', 'ers_enqueue_plugin_styles' );
 
 
+    /**
+    * Include ACF
+    */
+    add_filter(plugins_url('assets/admin/advanced-custom-fields', __FILE__), 'my_acf_settings_path');
+
+    function my_acf_settings_path( $path ) {
+
+        // update path
+        $path = get_stylesheet_directory() . '/acf/';
+
+        // return
+        return $path;
+
+    }
+
+
+    // 2. customize ACF dir
+    add_filter('advanced-custom-fields/settings/dir', 'my_acf_settings_dir');
+
+    function my_acf_settings_dir( $dir ) {
+
+        // update path
+        $dir = get_stylesheet_directory_uri() . '/acf/';
+
+        // return
+        return $dir;
+
+    }
+
+
+    // 3. Hide ACF field group menu item
+    add_filter('acf/settings/show_admin', '__return_false');
+
+
+    // 4. Include ACF
+    //include_once( get_stylesheet_directory() . '/assets/admin/advanced-custom-fields/acf.php' );
+
  ?>
